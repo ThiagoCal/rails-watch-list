6066bb767c863a24bf18fa734257bf83cb84d050
Thiago Calazans <thiagotorrescalazans@gmail.com>
2022-02-25 18:20:11 -0300

    changed routes photo

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,6 @@ class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, :dependent => :delete_all
   has_one_attached :photo
+  validates :photo, presence: true
   validates :name, presence: true, uniqueness: true
 end
